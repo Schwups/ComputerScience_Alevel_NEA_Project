61b783298e982dfb163e0960ee02288c8260fc4b
Schwups <sexton990@gmail.com>
2023-02-08 19:56:40 +0000

    Added highscore sub system
    Added system to convert high scores to and from json format

---
+++ ComputerScience_Alevel_NEA_Project/Minesweeper/HighScoreSystem.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighScoreSystem
+{
+    class HighScoreUtilities
+    {
+        const string HighScoreFileName = "HighScores";
+        static readonly string jsonPath = Directory.GetCurrentDirectory() + $@"\{HighScoreFileName}.json";
+        public static void UpdateHighScores(HighScore[] highScores)
+        {
+            if (highScores == null)
+            {
+                throw new ArgumentNullException("highScores parameter was null");
+            }
+            HighScoresArray highScoresArray = new HighScoresArray()
+            {
+                highScores = highScores
+            };
+            FileStream fileStream = File.OpenWrite(jsonPath);
+            using (StreamWriter sw = new StreamWriter(fileStream))
+            {
+                sw.Write(JsonConvert.SerializeObject(highScoresArray));
+            }
+        }
+        public static HighScore[] GetHighScores()
+        {
+            var highScoresArray = JsonConvert.DeserializeObject<HighScoresArray>(File.ReadAllText(jsonPath));
+            return highScoresArray.highScores;
+        }
+        private struct HighScoresArray
+        {
+            public HighScore[] highScores;
+        }
+    }
+    struct HighScore
+    {
+        public string userName;
+        public int time;
+        public DateTime date;
+    }
+}
