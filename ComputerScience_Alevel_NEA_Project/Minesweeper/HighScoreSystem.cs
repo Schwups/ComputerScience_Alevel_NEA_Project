8c02b3682ee7ccb46ba8ccb88fb18bce183f3741
Schwups <sexton990@gmail.com>
2023-02-09 21:05:37 +0000

    Added Merge sort algorithm
    Added merge sort algorithm to sort highscores by time

--- ComputerScience_Alevel_NEA_Project/Minesweeper/HighScoreSystem.cs
+++ ComputerScience_Alevel_NEA_Project/Minesweeper/HighScoreSystem.cs
@@ -30,8 +30,80 @@ namespace HighScoreSystem
         }
         public static HighScore[] GetHighScores()
         {
-            var highScoresArray = JsonConvert.DeserializeObject<HighScoresArray>(File.ReadAllText(jsonPath));
-            return highScoresArray.highScores;
+            try
+            {
+                var highScoresArray = JsonConvert.DeserializeObject<HighScoresArray>(File.ReadAllText(jsonPath));
+                return highScoresArray.highScores;
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw ex;
+            }
+        }
+        public static HighScore[] SortScoresByTime(HighScore[] array)
+        {
+            return MergeSortAlgorithm(array, 0, array.Length - 1);
+            HighScore[] MergeSortAlgorithm(HighScore[] arr, int left, int right)
+            {
+                if (left >= right)
+                {
+                    return arr;
+                }
+                int middle = (left + right) / 2;
+                MergeSortAlgorithm(arr, left, middle);
+                MergeSortAlgorithm(arr, middle + 1, right);
+                Merge(arr, left, middle, right);
+                return arr;
+            }
+            HighScore[] Merge(HighScore[] arr, int left, int middle, int right)
+            {
+                int leftArrayLength = middle - left + 1;
+                int rightArrayLength = right - middle;
+                HighScore[] leftArray = new HighScore[leftArrayLength];
+                HighScore[] righArray = new HighScore[rightArrayLength];
+
+                for (int i = 0; i < leftArrayLength; i++)
+                {
+                    leftArray[i] = arr[i + left];
+                }
+                for (int i = 0; i < rightArrayLength; i++)
+                {
+                    righArray[i] = arr[i + middle + 1];
+                }
+
+                int leftCounter = 0;
+                int rightCounter = 0;
+                int counter = left;
+                while (leftCounter < leftArrayLength && rightCounter < rightArrayLength)
+                {
+                    if (leftArray[leftCounter].time < righArray[rightCounter].time)
+                    {
+                        arr[counter] = leftArray[leftCounter];
+                        counter++;
+                        leftCounter++;
+                    }
+                    else
+                    {
+                        arr[counter] = righArray[rightCounter];
+                        counter++;
+                        rightCounter++;
+                    }
+                }
+
+                while (leftCounter < leftArrayLength)
+                {
+                    arr[counter] = leftArray[leftCounter];
+                    counter++;
+                    leftCounter++;
+                }
+                while (rightCounter < rightArrayLength)
+                {
+                    arr[counter] = righArray[rightCounter];
+                    counter++;
+                    rightCounter++;
+                }
+                return arr;
+            }
         }
         private struct HighScoresArray
         {
